73fdab291f731023833d3e80a2bccaa508dfd435
Prachi0Tiwari <91770870+Prachi0Tiwari@users.noreply.github.com>
2023-08-13 15:26:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// JavaScript code for portfolio website
+
+// Toggle color scheme
+const toggleButton = document.getElementById('toggle-button');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+  body.classList.toggle('dark-theme');
+});
+const projectCards = document.querySelectorAll('.project');
+
+
